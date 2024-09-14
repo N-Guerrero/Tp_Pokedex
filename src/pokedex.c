644ab84bea9bd132a385adf6a173220155b813d5
N-Guerrero <guerreronico81@gmail.com>
2024-09-14 00:00:01 -0300

    progreso

--- src/pokedex.c
+++ src/pokedex.c
@@ -1,24 +1,56 @@
 #include "pokedex.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct pokedex {
 	size_t cantidad_pokemons;
-	struct pokemon* Pokemons[];
+	struct pokemon** ptr_pokemons;
 };
 
 struct pokedex *pokedex_crear()
 {
-	return NULL;
+
+	
+	
+
+	struct pokedex *Pokedex=malloc(sizeof(struct pokedex));
+	if(Pokedex==NULL){
+		printf("error al asignar memoria a Pokedex");
+		return NULL;
+	}
+
+	Pokedex->cantidad_pokemons=0;
+	Pokedex->ptr_pokemons=(struct pokemon**)malloc(sizeof(struct pokemon*));
+	if(Pokedex->ptr_pokemons==NULL){
+		printf("error al asignar memoria a Pokedex->ptr_pokemons");
+		return NULL;
+	}
+	
+	return Pokedex;
 }
 
 bool pokedex_agregar_pokemon(struct pokedex *pokedex, struct pokemon pokemon)
 {
+	if (pokedex->cantidad_pokemons==0){
+		pokedex->ptr_pokemons[0]=&pokemon;
+		pokedex->cantidad_pokemons++;
+	}else{
+		pokedex->cantidad_pokemons++;
+		pokedex->ptr_pokemons=(struct pokemon**)realloc(pokedex->ptr_pokemons,(pokedex->cantidad_pokemons++)*(sizeof(struct pokemon*)));
+		if(pokedex->ptr_pokemons==NULL){
+			printf("error al reasignar memoria a Pokedex->ptr_pokemons");
+			return 1;
+		}
+		pokedex->ptr_pokemons[pokedex->cantidad_pokemons]=&pokemon;
+
+	}
 	return false;
 }
 
 size_t pokedex_cantidad_pokemones(struct pokedex *pokedex)
 {
-	return 0;
+	return pokedex->cantidad_pokemons;
 }
 
 const struct pokemon *pokedex_buscar_pokemon(struct pokedex *pokedex,
