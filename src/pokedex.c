117ebe050352629b06c20fbf86be1db164d81f97
N-Guerrero <guerreronico81@gmail.com>
2024-09-14 02:00:45 -0300

    funciones de pokedex completadas, falta probar

--- src/pokedex.c
+++ src/pokedex.c
@@ -7,12 +7,11 @@ struct pokedex {
 	size_t cantidad_pokemons;
 	struct pokemon** ptr_pokemons;
 };
+bool pokedex_ordenar(struct pokedex*);
+
 
 struct pokedex *pokedex_crear()
 {
-
-	
-	
 
 	struct pokedex *Pokedex=malloc(sizeof(struct pokedex));
 	if(Pokedex==NULL){
@@ -35,17 +34,18 @@ bool pokedex_agregar_pokemon(struct pokedex *pokedex, struct pokemon pokemon)
 	if (pokedex->cantidad_pokemons==0){
 		pokedex->ptr_pokemons[0]=&pokemon;
 		pokedex->cantidad_pokemons++;
+		return true;
 	}else{
 		pokedex->cantidad_pokemons++;
 		pokedex->ptr_pokemons=(struct pokemon**)realloc(pokedex->ptr_pokemons,(pokedex->cantidad_pokemons++)*(sizeof(struct pokemon*)));
 		if(pokedex->ptr_pokemons==NULL){
 			printf("error al reasignar memoria a Pokedex->ptr_pokemons");
-			return 1;
+			return false;
 		}
 		pokedex->ptr_pokemons[pokedex->cantidad_pokemons]=&pokemon;
-
+		return true;
 	}
-	return false;
+	if (pokedex==NULL)return false;
 }
 
 size_t pokedex_cantidad_pokemones(struct pokedex *pokedex)
@@ -56,7 +56,14 @@ size_t pokedex_cantidad_pokemones(struct pokedex *pokedex)
 const struct pokemon *pokedex_buscar_pokemon(struct pokedex *pokedex,
 					     const char *nombre)
 {
-	return NULL;
+
+	size_t id_buscado=0;
+	for(size_t i=0;i<pokedex->cantidad_pokemons;i++){
+		if(strcmp(nombre, pokedex->ptr_pokemons[i]->nombre)==0)
+		id_buscado=i;
+	}
+
+	return pokedex->ptr_pokemons[id_buscado];
 }
 
 
@@ -64,9 +71,47 @@ size_t pokedex_iterar_pokemones(struct pokedex *pokedex,
 				bool (*funcion)(struct pokemon *, void *),
 				void *ctx)
 {
-	return 0;
+	bool oredenado=pokedex_ordenar(pokedex);
+	if(oredenado==false){
+		printf("fallo al ordenar pokedex");
+		return 0;
+	}
+	size_t iteraciones=0;
+	for (size_t i=0;i<pokedex->cantidad_pokemons;i++){
+		if(funcion(pokedex->ptr_pokemons[i],ctx))
+			iteraciones++;
+	}
+
+	return iteraciones;
 }
 
 void pokedex_destruir(struct pokedex *pokedex)
 {
+	for(size_t i=0;i<pokedex->cantidad_pokemons;i++){
+		free(pokedex->ptr_pokemons[i]);
+	}
+	free(pokedex->ptr_pokemons);
+	if(pokedex->ptr_pokemons==NULL)printf("Pokemons liberados!!\n");
+	free(pokedex);
+	if(pokedex==NULL)printf("Pokedex eliminada\n");
+}
+
+
+bool pokedex_ordenar(struct pokedex* pokedex){
+
+	struct pokemon* aux=(struct pokemon*)malloc(sizeof(struct pokemon));
+	for(size_t i=0;i<(pokedex->cantidad_pokemons-1);i++){
+		size_t j_min=i;
+		for(size_t j=i+1;j<pokedex->cantidad_pokemons;j++){
+			if(strcmp(pokedex->ptr_pokemons[j]->nombre,pokedex->ptr_pokemons[j_min]->nombre)<0)
+				j_min=j;
+			if(j_min!=i){
+				aux=pokedex->ptr_pokemons[i];
+				pokedex->ptr_pokemons[i]=pokedex->ptr_pokemons[j_min];
+				pokedex->ptr_pokemons[j_min]=aux;
+			}
+		}
+	}
+	
+
 }
